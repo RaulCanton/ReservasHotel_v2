87399cb377ab072d141e75373cee638af3774325
Raúl Cantón <raulcantong@hotmail.com>
2023-12-12 11:36:23 +0100

    Primer commit

--- src/main/java/org/iesalandalus/programacion/reservashotel/dominio/Huesped.java
+++ src/main/java/org/iesalandalus/programacion/reservashotel/dominio/Huesped.java
@@ -1,4 +1,19 @@
 package org.iesalandalus.programacion.reservashotel.dominio;
 
+import java.time.LocalDate;
+
 public class Huesped {
+    private String ER_TELEFONO;
+    private String ER_CORREO;
+    private String ER_DNI;
+    public String FORMATO_FECHA;
+
+    private String nombre;
+    private String telefono;
+    private String correo;
+    private String dni;
+    private LocalDate fechaNacimiento;
+
+
+
 }
