cbba08efea70be7bb80e7a8bfa55784db53e2ea9
Raúl Cantón <raulcantong@hotmail.com>
2023-12-12 11:13:10 +0100

    Primer commit

--- src/main/java/org/iesalandalus/programacion/reservashotel/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/reservashotel/MainApp.java
@@ -2,7 +2,9 @@ package org.iesalandalus.programacion.reservashotel;
 
 
 public class MainApp {
-    
+
+
+
     public static void main(String[] args) {
         System.out.println("Hasta luego!!!!");
     }
