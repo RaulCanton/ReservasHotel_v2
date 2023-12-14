94f0ced4c40c71508b7033a334fb4e821893234d
Raúl Cantón <raulcantong@hotmail.com>
2023-12-14 12:09:04 +0100

    Primer commit

--- src/main/java/org/iesalandalus/programacion/reservashotel/dominio/Huesped.java
+++ src/main/java/org/iesalandalus/programacion/reservashotel/dominio/Huesped.java
@@ -14,6 +14,84 @@ public class Huesped {
     private String dni;
     private LocalDate fechaNacimiento;
 
+    public String getNombre() {
+    return nombre;
+    }
+
+    public void setNombre(String nombre){
+
+    }
+
+    private String formateaNombre(String nombre){
+
+        return nombre;
+    }
+
+    public String getTelefono(){
+        return telefono;
+    }
+
+    public void setTelefono(String telefono){
 
 
+    }
+    public String getCorreo(){
+        return correo;
+    }
+
+    public void setCorreo(String correo){
+
+
+    }
+
+    public String getDni(){
+        return dni;
+    }
+
+    private void setDni(String dni){
+
+
+    }
+
+    private boolean comprobarLetraDni(String dni){
+        boolean resultado = true;
+
+        return resultado;
+    }
+
+    private LocalDate getFechaNacimiento(){
+        return fechaNacimiento;
+    }
+
+    private void setFechaNacimiento(LocalDate fechaNacimiento){
+
+
+    }
+
+    private String getIniciales(){
+        String inicales="ABC";
+
+        return inicales;
+    }
+
+
+public Huesped(String nombre,String dni,String correo, String telefono,LocalDate fechaNacimiento){
+
+    setNombre(nombre);
+    setDni(dni);
+    setCorreo(correo);
+    setTelefono(telefono);
+    setFechaNacimiento(fechaNacimiento);
 }
+
+public Huesped (Huesped huesped){
+
+        setNombre(getNombre());
+        setDni(getDni());
+        setCorreo(getCorreo());
+        setTelefono(getTelefono());
+        setFechaNacimiento(getFechaNacimiento());
+}
+}
+
+
