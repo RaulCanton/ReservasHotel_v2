54ba1f5e84f908a22c7f7c5b069b5c6e3cf8c8bb
Raúl Cantón <raulcantong@hotmail.com>
2024-01-22 23:43:35 +0100

    Cambio en readme

--- src/main/java/org/iesalandalus/programacion/reservashotel/vista/Opcion.java
+++ src/main/java/org/iesalandalus/programacion/reservashotel/vista/Opcion.java
@@ -2,26 +2,39 @@ package org.iesalandalus.programacion.reservashotel.vista;
 
 public enum Opcion {
 
-    SALIR("ADIOS"),
+    SALIR("Salir"),
     INSERTAR_HUESPED("Inserte un huésped."),
-    BUSCAR_HUESPED("Introduzca dni de huésped a buscar."),
-    BORRAR_HUESPED("Introduzca dni de huésped a borrar."),
-    MOSTRAR_HUESPEDES(""),
-    INSERTAR_hABITACION(),
-    BUSCAR_HABITACION(),
-    BORRAR_HABITACION(),
-    MOSTRAR_HABITACIONES(),
-    INSERTAR_RESERVA(),
-    ANULAR_RESERVA(),
-    MOSTRAR_RESERVAS(),
-    CONSULTAR_DISPONIBIIDAD();
+    BUSCAR_HUESPED("Busca huésped."),
+    BORRAR_HUESPED("Borra huésped."),
+    MOSTRAR_HUESPEDES("Muestra huésped."),
+    INSERTAR_HABITACION("Inserta habitación."),
+    BUSCAR_HABITACION("Busca habitación."),
+    BORRAR_HABITACION("Borra habitación."),
+    MOSTRAR_HABITACIONES("Muestra habitaciones."),
+    INSERTAR_RESERVA("Inserta reserva."),
+    ANULAR_RESERVA("Anula reserva."),
+    MOSTRAR_RESERVAS("Muestra reserva."),
+    CONSULTAR_DISPONIBILIDAD("Consulta disponibilidad.");
 
-    String mensajeAMostrar;
+    private  String mensajeAMostrar;
     private Opcion(String mensajeAMostrar){
         this.mensajeAMostrar=mensajeAMostrar;
     }
 
-    public String toString(){
-        return mensajeAMostrar;
+
+    @Override
+    public String toString() {
+        return String.format("%d.- %s", ordinal(), mensajeAMostrar);
+    }
+
+    public static Opcion getOpcionSegunOrdinal(int ordinal) {
+        if (esOrdinalValido(ordinal))
+            return values()[ordinal];
+        else
+            throw new IllegalArgumentException("Ordinal de la opción no válido");
+    }
+
+    public static boolean esOrdinalValido(int ordinal) {
+        return (ordinal >= 0 && ordinal <= values().length - 1);
     }
 }
